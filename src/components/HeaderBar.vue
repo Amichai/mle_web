771c41a8db342c2dab40d6b89f2b2e53ee83e607
Amichai Levy <amichailevy@Amichais-MacBook-Pro.local>
2023-12-02 15:33:35 -0500

    initial commit

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, onMounted, computed, nextTick, watch } from 'vue'
+import logo from '@/assets/logo.png'
+
+
+const emits = defineEmits([])
+</script>
+
+<template>
+  <div class="header">
+      <img :src="logo" alt="money line edge" class="logo" height="40">
+
+      <!-- <div class="title">
+        Money Line Edge
+      </div> -->
+  </div>
+</template>
+
+<style>
+
+.header {
+  background-color: maroon;
+  text-align: center;
+  display: flex;
+}
+
+.title {
+  color: #040000;
+  text-align: center;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Inter;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.logo {
+  margin: 0.3rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HeaderBar from '../components/HeaderBar.vue';
+</script>
+
+<template>
+  <main>
+    <HeaderBar />
+    teast
+  </main>
+</template>
